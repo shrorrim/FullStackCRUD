0ebf074a75d6bd22d281e2e77d63675a874abf93
Nagy Marcell <shrorrim@gmail.com>
2023-10-14 17:06:04 +0200

    Add project files.

---
+++ X4GA1C_HFT_2023241.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace X4GA1C_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
