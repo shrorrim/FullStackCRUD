f402fd2bc8a824bac1941aba8de6e34e1505b369
Nagy Marcell <shrorrim@gmail.com>
2023-10-16 14:10:12 +0200

    Started creating dbcontex

--- X4GA1C_HFT_2023241.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace X4GA1C_HFT_2023241.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ X4GA1C_HFT_2023241.Repository/LaptopWebShopDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using X4GA1C_HFT_2023241.Models;
+
+namespace X4GA1C_HFT_2023241.Repository
+{
+    public class LaptopWebShopDbContext : DbContext
+    {
+
+        // data sets:
+
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Laptop> Laptops { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+
+        public LaptopWebShopDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (builder.IsConfigured == false)
+            {
+                // most még localdb vel de majd ---> in memory database!
+                string connectionStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\LocalDataBase.mdf;Integrated Security=True";
+                builder.UseSqlServer(connectionStr)
+                    .UseLazyLoadingProxies();
+            }
+        }
+
+    }
+}
